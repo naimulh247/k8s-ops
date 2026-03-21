d66ec8df9e01de41767bc63da073868d3a535e96
naimulh247 <naimulh247@gmail.com>
2026-03-21 14:27:21 -0400

    add RBAC permissions for events, deployments, and pods in Memcached controller

--- memcached-operator/internal/controller/memcached_controller.go
+++ memcached-operator/internal/controller/memcached_controller.go
@@ -46,6 +46,9 @@ type MemcachedReconciler struct {
 // +kubebuilder:rbac:groups=cache.nai-k8s-ops.com,resources=memcacheds,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=cache.nai-k8s-ops.com,resources=memcacheds/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=cache.nai-k8s-ops.com,resources=memcacheds/finalizers,verbs=update
+// +kubebuilder:rbac:groups=events.k8s.io,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
