4a0cc17c532957a0679370a1f43b9ca616a67145
naimulh247 <naimulh247@gmail.com>
2026-03-22 15:03:33 -0400

    add controller imports

--- cronjob-operator/internal/controller/cronjob_controller.go
+++ cronjob-operator/internal/controller/cronjob_controller.go
@@ -18,8 +18,19 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"maps"
+	"slices"
+	"time"
 
+	"github.com/robfig/cron"
+    kbatch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+    apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ref "k8s.io/client-go/tools/reference"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
